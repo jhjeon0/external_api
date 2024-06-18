d9fd44d542d46691f826cd877561f3c0f74fae04
jhjeon <powwow94@hanmail.net>
2024-06-18 09:02:38 +0900

    feat: add weather-api

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # external_api
+
 외부 api test
+
+# 날씨 api 링크
+
+- [공공 데이터 포털](https://www.data.go.kr/)
+- [open-weather-api](https://openweathermap.org/)
---
+++ weather/open_weather_api.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+api_key = ""
+lat = "37.49"
+lot = "126.88"
+lang = "kr"
+city = "Seoul"
+
+api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang={lang}&units=metric"
+
+result = requests.get(api)
+data = json.loads(result.text)
+
+
+print(f"{data['name']}의 날씨입니다.")
+print(f"날씨는 {data["weather"][0]['description']}입니다.")
+
+'''
+data["main"]["temp"]
+data["main"]["feels_like"]
+data["main"]["temp_min"]
+data["main"]["temp_max"]
+data["main"]["humidity"]
+data["main"]["pressure"]
+data["wind"]["deg"]
+data["wind"]["speed"]
+'''
